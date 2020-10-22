bdf52ed0317e1f4582f6e946f67d4e5d3be704cd
Cristian Jimenez <cri.jimenez21@gmail.com>
2020-10-22 16:02:21 -0300

    Se trabajo en formularios
    Se agrego un formulario de contacto
    Se agregaron varias planillas
    Se modico las vistas y urls

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 
 # Create your views here.
 
@@ -9,6 +10,34 @@ def busqueda_productos(request):
 
 def buscar(request):
 
-    mensaje="Articulo buscado: %r" %request.GET["prd"]
 
-    return HttpResponse(mensaje)
+    if request.GET["prd"]:
+        
+        #mensaje="Articulo buscado: %r" %request.GET["prd"]
+        producto=request.GET["prd"]
+
+        if len(producto)>20:
+
+            mensaje="texto de busqueda demasiado grande"
+
+        else:
+        
+
+            articulos=Articulos.objects.filter(nombre__icontains=producto)
+
+            return render(request, "resultados_busqueda.html", {"articulos":articulos, "query":producto})
+
+    else:
+
+        mensaje="No has introduce nada"
+
+    return HttpResponse(mensaje) 
+
+def contacto(request):
+
+    if request.method=="POST":
+
+        return render(request, "gracias.html")  
+
+    return render(request, "contacto.html")   
+    
